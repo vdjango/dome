167d1d6d5618432541013d5ba45a23a70f5b3059
君思 <1748011755@qq.com>
2021-03-10 17:19:54 +0800

    first commit

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
-
 # Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class UpdateAPITestCase(APITestCase):
+    """单元测试"""
+    def test_get_update(self):
+        response = self.client.get('/app/update/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_update(self):
+        response = self.client.get('/app/update/', data={"uid": 2, "number": 90}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class rankingAPITestCase(APITestCase):
+    """单元测试"""
+    def test_get_ranking(self):
+        response = self.client.get('/app/ranking/', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_ranking_limt(self):
+        response = self.client.get('/app/ranking/?uid=5', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
